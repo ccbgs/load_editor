fcd91ab076f2bee739e0bb5ac1107d902abb3d0f
ccbgs <chad.conger@bgrsys.com>
2014-09-09 18:03:13 -0500

    Removed javascript
    Removed javascript from the load_editor_new_editor() function

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?
-/* load custom editor template */
+// load custom editor template 
 function load_editor_template( $template )
 {
     $template = plugin_dir_path( __FILE__ ) . 'template.php';
@@ -9,36 +9,30 @@ function load_editor_template( $template )
 
 add_filter( 'template_include', 'load_editor_template' );
 
-/* load javascript */
+// load javascript 
 function load_editor_scripts() {
-	wp_enqueue_script( 'load_editor', plugins_url() . '/load_editor/js/load_editor.js', array(), '', true );
-	wp_enqueue_script( 'jquery');
+    wp_enqueue_script( 'load_editor', plugins_url() . '/load_editor/js/load_editor.js', array(), '', true );
+    wp_enqueue_script( 'jquery');
 }
 
 add_action( 'wp_enqueue_scripts', 'load_editor_scripts' );
 
-/* create new editor */
+// create new editor
 function load_editor_new_editor() {
-	$id      = $_POST['id'];
-	$number  = $_POST['number'];
-	$next    = $number + 1;
-	$full_id = $id.$number;
-	
-	echo "<h1>Editor $number</h1>";
-	
-	$content = '<p>This is example content.</p>';
-	wp_editor($content, $full_id, array('textarea_rows' => 3));
-	
-	// initiate new editor
-	echo "<script>
-tinyMCE.execCommand('mceAddEditor', true, $full_id);
-tinyMCE.init(tinyMCEPreInit.mceInit[$full_id]);
-</script>";
+    $id      = $_POST['id'];
+    $number  = $_POST['number'];
+    $next    = $number + 1;
+    $full_id = $id.$number;
 
-	// create "add new" text
-	echo "<div><a onclick=\"load_new_editor('editor', $next);\" href='javascript:void(0);'>Click here</a> to add another editor</div>";
-	
-	die();
+    echo "<h1>Editor $number</h1>";
+
+    $content = '<p>This is example content.</p>';
+    wp_editor($content, $full_id, array('textarea_rows' => 3));
+
+    // create "add new" text
+    echo "<div><a onclick=\"load_new_editor('editor', $next);\" href='javascript:void(0);'>Click here</a> to add another editor</div>";
+
+    die();
 }
 
 add_action( 'wp_ajax_load_editor_new_editor', 'load_editor_new_editor' );

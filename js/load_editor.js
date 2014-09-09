df98176a55c8ce70eda90a77dbf9e42b1a67b31b
ccbgs <chad.conger@bgrsys.com>
2014-09-09 18:05:29 -0500

    Added editor Initiation here
    Added quicktags() to make tabs work
    
    Added settings to match wordpress editor settings

--- js/load_editor.js
+++ js/load_editor.js
@@ -17,10 +17,60 @@ function load_new_editor(id, number){
 		//add new editor
 		jQuery('#editor_container').append(response);
         
-		
-		//tinyMCE.execCommand('mceAddEditor', true, fullId);
+		// this is need for the tabs to work
+		quicktags({id : fullId});
 
-		//tinyMCE.init(tinyMCEPreInit.mceInit[fullId]);
+		// use wordpress settings
+		tinymce.init({
+        selector: fullId,
+
+        theme:"modern",
+		skin:"lightgray",
+		language:"en",
+		formats:{
+						alignleft: [
+							{selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li', styles: {textAlign:'left'}},
+							{selector: 'img,table,dl.wp-caption', classes: 'alignleft'}
+						],
+						aligncenter: [
+							{selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li', styles: {textAlign:'center'}},
+							{selector: 'img,table,dl.wp-caption', classes: 'aligncenter'}
+						],
+						alignright: [
+							{selector: 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li', styles: {textAlign:'right'}},
+							{selector: 'img,table,dl.wp-caption', classes: 'alignright'}
+						],
+						strikethrough: {inline: 'del'}
+					},
+					relative_urls:false,
+					remove_script_host:false,
+					convert_urls:false,
+					browser_spellcheck:true,
+					fix_list_elements:true,
+					entities:"38,amp,60,lt,62,gt",
+					entity_encoding:"raw",
+					keep_styles:false,
+					paste_webkit_styles:"font-weight font-style color",
+					preview_styles:"font-family font-size font-weight font-style text-decoration text-transform",
+					wpeditimage_disable_captions:false,
+					wpeditimage_html5_captions:true,
+					plugins:"charmap,hr,media,paste,tabfocus,textcolor,fullscreen,wordpress,wpeditimage,wpgallery,wplink,wpdialogs,wpview",
+					selector:"#" + fullId,
+					resize:"vertical",
+					menubar:false,
+					wpautop:true,
+					indent:false,
+					toolbar1:"bold,italic,strikethrough,bullist,numlist,blockquote,hr,alignleft,aligncenter,alignright,link,unlink,wp_more,spellchecker,fullscreen,wp_adv",toolbar2:"formatselect,underline,alignjustify,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help",
+					toolbar3:"",
+					toolbar4:"",
+					tabfocus_elements:":prev,:next",
+					body_class:"id post-type-post post-status-publish post-format-standard",
+					
+});
+
+		
+		// this is needed for the editor to initiate
+		tinyMCE.execCommand('mceAddEditor', false, fullId); 
 	   
 	});
 }
